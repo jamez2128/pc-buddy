3a74c310fd9e981087f7de8aa514b12f66ec7c9f
jamez2128 <jamez2128@ArchPredator.localdomain>
2022-06-06 01:23:30 +0800

    Basic Template

---
+++ android/app/src/main/kotlin/com/tip/it12s8/group3/pc_buddy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tip.it12s8.group3.pc_buddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "PC Buddy",
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const Home()
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ String appBarTitle = "Learn";
+ int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(title: Text(appBarTitle),backgroundColor: Colors.purple),
+      drawer:
+        Drawer(
+          backgroundColor: Color(0xff0d0029),
+          child: ListView(
+            children: [
+              navBarChoices("Learn"),
+              navBarChoices("Referrences"),
+              navBarChoices("Credits"),
+              navBarChoices("Feedback")
+            ],
+          ),
+      ),
+      backgroundColor: Color(0xff0d0029),
+    );
+  }
+}
+
+ListTile navBarChoices(String title) {
+  return ListTile(
+    title: Text(title,
+      style: TextStyle(color: Colors.white),),
+    onTap: () {print("Hello World");} ,
+  );
+}
