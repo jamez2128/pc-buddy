272ca80fd7bf2bf6ec3af9a759d50f18b5949a61
Wesley Emerenciana <Ahdrianne>
2022-06-07 02:51:19 +0800

    Final credits done // ongoing references and introduction

--- lib/references.dart
+++ lib/references.dart
@@ -5,11 +5,38 @@ class ReferencesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-        "References goes here",
-      style: TextStyle(
-        color: Colors.white,
+    return SingleChildScrollView(
+      child: Container(
+        child: Center(
+          child: Column(
+            children: [
+
+
+              Container(
+                margin: const EdgeInsets.all(10.0),
+                color: Colors.white,
+                padding: const EdgeInsets.all(10.0),
+                child:  const Text(
+                  'What Computer Parts do you need to build a PC,\n'
+                      'you ask? Does this mean you want to build\n'
+                  'your own PC? That is absolutely splendid!\n\n'
+
+                'Building your own Computer from individual PC Components\n'
+                  'has so many benefits compared to just going \n'
+                      'out and buying a pre-built PC\n'
+                  ,
+                  textAlign: TextAlign.center,
+
+                ),
+              ),
+
+
+
+            ],
+          ),
+        ),
       ),
     );
+
   }
 }
